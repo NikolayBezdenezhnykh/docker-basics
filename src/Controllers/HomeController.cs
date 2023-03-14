aab3bae95798a31c59e2dda1cc0beb1bc9e7c391
Nikolay Bezdenezhnykh <nikolai200290@gmail.com>
2023-03-14 18:41:42 +0300

    Создал тестовое приложение

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DockerBasics.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Content("Description");
+        }
+    }
+}
